6c09f5560f8c00e5153687fff7c5a9cf3013a658
Hotaru <biyuehuya@gmail.com>
2025-01-01 10:12:41 +0800

    chore: update example code

--- example/example1.ts
+++ example/example1.ts
@@ -1,10 +1,10 @@
-import Tsu, { tsuFactory } from '../src';
+import Tsu, { tsuFactory } from '../src'
 
-const schema = Tsu.Tuple([Tsu.Number()]);
-export type Schema = Tsu.infer<typeof schema>;
+const schema = Tsu.Tuple([Tsu.Number()])
+export type Schema = Tsu.infer<typeof schema>
 
-const schema2 = Tsu.Array(Tsu.String());
-export type Schema2 = Tsu.infer<typeof schema2>;
+const schema2 = Tsu.Array(Tsu.String())
+export type Schema2 = Tsu.infer<typeof schema2>
 
 const schema3 = Tsu.Object({
   value: Tsu.Number(),
@@ -12,26 +12,23 @@ const schema3 = Tsu.Object({
   host: Tsu.String().regexp(/http(s)?:\/\/(.*)/),
   port: Tsu.Number().range(1, 65565).int(),
   allowList: Tsu.Array(Tsu.String()),
-  listType: Tsu.Union([Tsu.Literal('include'), Tsu.Literal('exclude')])
-});
+  listType: Tsu.Union(Tsu.Literal('include'), Tsu.Literal('exclude'))
+})
 
-export type Schema3 = Tsu.infer<typeof schema3>;
+export type Schema3 = Tsu.infer<typeof schema3>
 
-const schema4 = Tsu.Intersection([Tsu.Number(), Tsu.Literal(1)]);
-export type Schema4 = Tsu.infer<typeof schema4>;
+const schema4 = Tsu.Intersection(Tsu.Number(), Tsu.Literal(1))
+export type Schema4 = Tsu.infer<typeof schema4>
 
-const schema5 = Tsu.Intersection([
-  Tsu.Literal('hello world'),
-  Tsu.Union([schema, Tsu.Union([Tsu.Number().optional(), schema2])])
-]);
-export type Schema5 = Tsu.infer<typeof schema5>;
+const schema5 = Tsu.Intersection(Tsu.Literal('hello world'), schema, Tsu.Number().optional(), schema2)
+export type Schema5 = Tsu.infer<typeof schema5>
 
 const schema6 = Tsu.Object({}).index(
   Tsu.String().regexp(/[0-9]+\.[0-9]+\.[0-9]+/),
   Tsu.String().regexp(/kotori-plugin-(.*)/)
-);
+)
 
-export type Schema6 = Tsu.infer<typeof schema6>;
+export type Schema6 = Tsu.infer<typeof schema6>
 export const example6: Schema6 = {
   'kotori-plugin-adapter-qq': '1.5.0',
   'kotori-plugin-adapter-wechat': '0.2.0',
@@ -39,39 +36,42 @@ export const example6: Schema6 = {
   'kotori-plugin-database-mysql': '3.1.0',
   'kotori-plugin-help': '1.2.0',
   'kotori-plugin-wiki': '1.0.0'
-};
+}
 
-const newTsu = tsuFactory('ja_JP');
+// Create your own tsuFactory with your own locale
+const newTsu = tsuFactory('zh_CN')
 
-export const localeTypeSchema = newTsu.Union([
-  newTsu.Union([newTsu.Literal('en_US'), newTsu.Literal('ja_JP')]),
-  newTsu.Union([newTsu.Literal('zh_CN'), newTsu.Literal('zh_TW')])
-]);
+export const localeTypeSchema = newTsu.Union(
+  newTsu.Literal('en_US'),
+  newTsu.Literal('ja_JP'),
+  newTsu.Literal('zh_CN'),
+  newTsu.Literal('zh_TW')
+)
 
 const globalConfigBaseSchema = newTsu.Object({
   lang: localeTypeSchema.default('ja_JP'),
   'command-prefix': newTsu.String().default('/')
-});
+})
 
-const adapterConfigBaseSchema = newTsu.Intersection([
+const adapterConfigBaseSchema = newTsu.Intersection(
   newTsu.Object({
     extends: newTsu.String(),
-    master: newTsu.Union([newTsu.Number(), newTsu.String()])
+    master: newTsu.Union(newTsu.Number(), newTsu.String())
   }),
   globalConfigBaseSchema
-]);
+)
 
 export const globalConfigSchema = newTsu.Object({
   global: globalConfigBaseSchema,
   adapter: newTsu.Object({}).index(adapterConfigBaseSchema).default({}),
   plugin: newTsu.Object({}).index(newTsu.Unknown()).default({})
-});
+})
 
-export type GlobalConfig = Tsu.infer<typeof globalConfigSchema>;
+export type GlobalConfig = Tsu.infer<typeof globalConfigSchema>
 
 console.log(
   globalConfigSchema.parse({
     global: { lang: 'zh_CN' },
-    adapter: { aa: { master: '1', lang: 'ja_JP' } }
+    adapter: { aa: { extends: 'bb', master: '1', lang: 'ja_JP' } }
   })
-);
+)
